d8a085f32c60c49d4bda8495042e2e580b252d44
YunChan-Oh <oyunchan9@gmail.com>
2024-12-11 03:14:01 +0900

    init

---
+++ src/main/java/com/oceaneeda/server/OceanEedaServerApplication.java
@@ -0,0 +1,13 @@
+package com.oceaneeda.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanEedaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanEedaServerApplication.class, args);
+	}
+
+}
