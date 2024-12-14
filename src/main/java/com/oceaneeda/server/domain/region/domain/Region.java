9e14d03539f2e3cd6f46626e513206e7e1d8371d
YunChan-Oh <oyunchan9@gmail.com>
2024-12-15 00:25:18 +0900

    feat(region): Region

---
+++ src/main/java/com/oceaneeda/server/domain/region/domain/Region.java
@@ -0,0 +1,21 @@
+package com.oceaneeda.server.domain.region.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "regions")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Region {
+
+    @Id
+    private ObjectId id;
+
+    @Indexed(unique = true)
+    private String name;
+}
