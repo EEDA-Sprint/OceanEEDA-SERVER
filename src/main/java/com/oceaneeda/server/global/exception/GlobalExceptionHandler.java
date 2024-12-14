56ba22aac6fe08f2f05f7c08d94a558ba4142ad5
YunChan-Oh <oyunchan9@gmail.com>
2024-12-15 00:31:23 +0900

    feat(exception): GlobalExceptionHandler

---
+++ src/main/java/com/oceaneeda/server/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.oceaneeda.server.global.exception;
+
+import graphql.GraphQLError;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GlobalExceptionHandler extends DataFetcherExceptionResolverAdapter {
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        if (ex instanceof EntityNotFoundException) {
+            return GraphQLError.newError()
+                    .errorType(ErrorType.NOT_FOUND)
+                    .message(ex.getMessage())
+                    .build();
+        } else if (ex instanceof IllegalArgumentException) {
+            return GraphQLError.newError()
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .message(ex.getMessage())
+                    .build();
+        }
+        return null;
+    }
+}
