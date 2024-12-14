9d1081be57320d6f5beecc6eb82a9589520de33a
YunChan-Oh <oyunchan9@gmail.com>
2024-12-15 00:24:40 +0900

    feat(marking): MarkingRepository

---
+++ src/main/java/com/oceaneeda/server/domain/marking/domain/repository/MarkingRepository.java
@@ -0,0 +1,10 @@
+package com.oceaneeda.server.domain.marking.domain.repository;
+
+import com.oceaneeda.server.domain.marking.domain.Marking;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MarkingRepository extends MongoRepository<Marking, ObjectId> {
+}
