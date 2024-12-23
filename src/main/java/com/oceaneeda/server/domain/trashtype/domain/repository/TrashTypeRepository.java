e12e61f2489bee923cf6589257478e0191267216
YunChan-Oh <oyunchan9@gmail.com>
2024-12-23 12:31:19 +0900

    rename(trashtype): 패키지명 변경

--- src/main/java/com/oceaneeda/server/domain/trashtype/domain/repository/TrashTypeRepository.java
+++ src/main/java/com/oceaneeda/server/domain/trashtype/domain/repository/TrashTypeRepository.java
@@ -1,6 +1,6 @@
-package com.oceaneeda.server.domain.trash.domain.repository;
+package com.oceaneeda.server.domain.trashtype.domain.repository;
 
-import com.oceaneeda.server.domain.trash.domain.TrashType;
+import com.oceaneeda.server.domain.trashtype.domain.TrashType;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
