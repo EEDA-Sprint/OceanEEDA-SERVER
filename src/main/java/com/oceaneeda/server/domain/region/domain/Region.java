a71c597bfc03088ce0eb23c5b12258e554d92c3a
YunChan-Oh <oyunchan9@gmail.com>
2024-12-23 12:30:10 +0900

    fix(region): setter 추가 및 생성자 접근 해제

--- src/main/java/com/oceaneeda/server/domain/region/domain/Region.java
+++ src/main/java/com/oceaneeda/server/domain/region/domain/Region.java
@@ -3,6 +3,7 @@ package com.oceaneeda.server.domain.region.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -10,7 +11,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "regions")
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Region {
 
     @Id
