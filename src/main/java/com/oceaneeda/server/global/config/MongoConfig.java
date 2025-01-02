77d5cfddc7b5dba2db5fd3eb10f8662d1cf2507a
YunChan-Oh <oyunchan9@gmail.com>
2025-01-03 01:38:03 +0900

    fix(date): MongoConfig 커스텀 Date 컨버터 추가

--- src/main/java/com/oceaneeda/server/global/config/MongoConfig.java
+++ src/main/java/com/oceaneeda/server/global/config/MongoConfig.java
@@ -1,9 +1,28 @@
 package com.oceaneeda.server.global.config;
 
+import com.oceaneeda.server.global.converter.DateToLocalDateTimeKstConverter;
+import com.oceaneeda.server.global.converter.LocalDateTimeToDateKstConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
+
+    @Bean
+    public MongoCustomConversions customConversions(
+            LocalDateTimeToDateKstConverter localDateTimeToDateKstConverter,
+            DateToLocalDateTimeKstConverter dateToLocalDateTimeKstConverter
+    ) {
+        return new MongoCustomConversions(
+                Arrays.asList(
+                        localDateTimeToDateKstConverter,
+                        dateToLocalDateTimeKstConverter
+                )
+        );
+    }
 }
