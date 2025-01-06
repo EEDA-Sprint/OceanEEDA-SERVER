53f91707efffeacf55e841dcd5a3ce335e23c946
YunChan-Oh <oyunchan9@gmail.com>
2025-01-06 11:16:07 +0900

    feat(auth): AccessDeniedException

---
+++ src/main/java/com/oceaneeda/server/global/exception/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package com.oceaneeda.server.global.exception;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
