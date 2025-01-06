e9f1137f1295bc6573ecd044905a575b2bb0c114
YunChan-Oh <oyunchan9@gmail.com>
2025-01-06 11:20:54 +0900

    feat(marking): MarkingRepository 존재여부 JPA 추가

--- src/main/java/com/oceaneeda/server/domain/marking/domain/repository/MarkingRepository.java
+++ src/main/java/com/oceaneeda/server/domain/marking/domain/repository/MarkingRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MarkingRepository extends MongoRepository<Marking, ObjectId> {
+    boolean existsByIdAndUserId(ObjectId id, String userId);
 }
