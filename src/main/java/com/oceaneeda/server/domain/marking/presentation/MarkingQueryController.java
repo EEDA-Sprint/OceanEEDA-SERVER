9651c15c02ef32a7f2003a5aaeb3483807d00c83
Yunchan-Oh <oyunchan9@gmail.com>
2025-01-18 19:55:01 +0900

    feat(marking): MarkingQueryController 유저별 마킹 조회 구분 추가

--- src/main/java/com/oceaneeda/server/domain/marking/presentation/MarkingQueryController.java
+++ src/main/java/com/oceaneeda/server/domain/marking/presentation/MarkingQueryController.java
@@ -1,8 +1,11 @@
 package com.oceaneeda.server.domain.marking.presentation;
 
+import com.oceaneeda.server.domain.auth.repository.AuthRepository;
 import com.oceaneeda.server.domain.marking.domain.Marking;
 import com.oceaneeda.server.domain.marking.domain.repository.MarkingRepository;
 import com.oceaneeda.server.domain.marking.presentation.dto.response.MarkingResponse;
+import com.oceaneeda.server.domain.user.domain.User;
+import com.oceaneeda.server.domain.user.domain.value.Role;
 import com.oceaneeda.server.global.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
@@ -11,13 +14,13 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 public class MarkingQueryController {
 
     private final MarkingRepository markingRepository;
+    private final AuthRepository authRepository;
 
     @QueryMapping
     public MarkingResponse getMarkingById(@Argument ObjectId id) {
@@ -28,8 +31,18 @@ public class MarkingQueryController {
 
     @QueryMapping
     public List<MarkingResponse> getAllMarkings() {
-        return markingRepository.findAll()
-                .stream()
+        User user = authRepository.getNullableCurrentUser();
+        List<Marking> markingList;
+
+        if (user == null) {
+            markingList = markingRepository.findByIsApprovedTrue();
+        } else if (user.getRole() == Role.ROLE_ADMIN) {
+            markingList = markingRepository.findAll();
+        } else {
+            markingList = markingRepository.findByIsApprovedTrueOrUserId(user.getId().toHexString());
+        }
+
+        return markingList.stream()
                 .map(MarkingResponse::from)
                 .toList();
     }
