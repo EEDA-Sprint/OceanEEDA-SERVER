78a6b6e474b56c5002402befde502709bf4b9c22
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-02 00:36:36 +0900

    fix(marking): GeoJsonPointResponse X, Y 순서에 맞도록 수정

--- src/main/java/com/oceaneeda/server/domain/marking/presentation/dto/response/GeoJsonPointResponse.java
+++ src/main/java/com/oceaneeda/server/domain/marking/presentation/dto/response/GeoJsonPointResponse.java
@@ -1,7 +1,17 @@
 package com.oceaneeda.server.domain.marking.presentation.dto.response;
 
+import lombok.Builder;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+
+@Builder
 public record GeoJsonPointResponse(
         Float latitude,
         Float longitude
 ) {
+    public static GeoJsonPointResponse from(GeoJsonPoint location) {
+        return GeoJsonPointResponse.builder()
+                .latitude((float) location.getY())
+                .longitude((float) location.getX())
+                .build();
+    }
 }
