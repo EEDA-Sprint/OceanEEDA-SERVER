95839c85775d744aab4f1107037ae07b0b083bc1
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:25:00 +0900

    refactor(auth): 코드 리팩토링

--- src/main/java/com/oceaneeda/server/domain/auth/presentation/dto/request/LoginInput.java
+++ src/main/java/com/oceaneeda/server/domain/auth/presentation/dto/request/LoginInput.java
@@ -1,7 +1,12 @@
 package com.oceaneeda.server.domain.auth.presentation.dto.request;
 
+import com.oceaneeda.server.domain.auth.service.command.LoginCommand;
+
 public record LoginInput(
         String email,
         String password
 ) {
+    public LoginCommand toCommand() {
+        return new LoginCommand(email, password);
+    }
 }
