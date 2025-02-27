00cad25ea099618e35dc7ae8e9903fe3aace69c8
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:26:02 +0900

    remove(auth): JwtUtil

--- src/main/java/com/oceaneeda/server/domain/auth/util/JwtUtil.java
+++ src/main/java/com/oceaneeda/server/domain/auth/util/JwtUtil.java
@@ -1,102 +1,102 @@
-package com.oceaneeda.server.domain.auth.util;
-
-import com.oceaneeda.server.domain.user.domain.value.Role;
-import com.oceaneeda.server.global.exception.ExpiredTokenException;
-import com.oceaneeda.server.global.exception.InvalidTokenException;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-
-@Component
-public class JwtUtil {
-
-    private final SecretKey secretKey;
-//    private final RefreshRepository refreshRepository;
-    @Value("${spring.jwt.access.expiration}")
-    private long accessTokenExpiration;
-    @Value("${spring.jwt.refresh.expiration}")
-    private long refreshTokenExpiration;
-
-    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
-        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
-    }
-
-    public String getId(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("sub", String.class);
-    }
-
-    public void shouldRefreshTokenValid(String token) {
-        try {
-            Jwts.parser()
-                    .verifyWith(secretKey)
-                    .build()
-                    .parse(token);
-        } catch (ExpiredJwtException e) {
-            throw new ExpiredTokenException("Expired token");
-        } catch (JwtException e) {
-            throw new InvalidTokenException("Invalid token");
-        }
-    }
-
-
-//    public String getCategory(String token) {
+//package com.oceaneeda.server.domain.auth.util;
+//
+//import com.oceaneeda.server.domain.user.domain.value.Role;
+//import com.oceaneeda.server.global.exception.ExpiredTokenException;
+//import com.oceaneeda.server.global.exception.InvalidTokenException;
+//import io.jsonwebtoken.ExpiredJwtException;
+//import io.jsonwebtoken.JwtException;
+//import io.jsonwebtoken.Jwts;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import javax.crypto.SecretKey;
+//import javax.crypto.spec.SecretKeySpec;
+//import java.nio.charset.StandardCharsets;
+//import java.util.Date;
+//
+//@Component
+//public class JwtUtil {
+//
+//    private final SecretKey secretKey;
+////    private final RefreshRepository refreshRepository;
+//    @Value("${spring.jwt.access.expiration}")
+//    private long accessTokenExpiration;
+//    @Value("${spring.jwt.refresh.expiration}")
+//    private long refreshTokenExpiration;
+//
+//    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+//        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+//    }
+//
+//    public String getId(String token) {
+//        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("sub", String.class);
+//    }
+//
+//    public void shouldRefreshTokenValid(String token) {
 //        try {
-//            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+//            Jwts.parser()
+//                    .verifyWith(secretKey)
+//                    .build()
+//                    .parse(token);
 //        } catch (ExpiredJwtException e) {
-//            return e.getClaims().get("category", String.class);
+//            throw new ExpiredTokenException("Expired token");
+//        } catch (JwtException e) {
+//            throw new InvalidTokenException("Invalid token");
 //        }
 //    }
 //
-//    public Role getRole(String token) {
-//        String roleValue = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
-//        return Role.fromValue(roleValue);
+//
+////    public String getCategory(String token) {
+////        try {
+////            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+////        } catch (ExpiredJwtException e) {
+////            return e.getClaims().get("category", String.class);
+////        }
+////    }
+////
+////    public Role getRole(String token) {
+////        String roleValue = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+////        return Role.fromValue(roleValue);
+////    }
+////
+////    public String getLoginType(String token) {
+////        try {
+////            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("loginType", String.class);
+////        } catch (ExpiredJwtException e) {
+////            return e.getClaims().get("loginType", String.class);
+////        }
+////    }
+//
+//    public String createAccessToken(String id, Role role, String loginType) {
+//        return createJwt("access", id, role, loginType, accessTokenExpiration);
 //    }
 //
-//    public String getLoginType(String token) {
-//        try {
-//            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("loginType", String.class);
-//        } catch (ExpiredJwtException e) {
-//            return e.getClaims().get("loginType", String.class);
-//        }
+//    public String createRefreshToken(String id, Role role, String loginType) {
+//        return createJwt("refresh", id, role, loginType, refreshTokenExpiration);
 //    }
-
-    public String createAccessToken(String id, Role role, String loginType) {
-        return createJwt("access", id, role, loginType, accessTokenExpiration);
-    }
-
-    public String createRefreshToken(String id, Role role, String loginType) {
-        return createJwt("refresh", id, role, loginType, refreshTokenExpiration);
-    }
-
-//    public void addRefreshToken(Long userId, String refreshToken) {
 //
-//        Date date = new Date(System.currentTimeMillis() + refreshTokenExpiration);
+////    public void addRefreshToken(Long userId, String refreshToken) {
+////
+////        Date date = new Date(System.currentTimeMillis() + refreshTokenExpiration);
+////
+////        Refresh refresh = Refresh.builder()
+////                .userId(userId)
+////                .refreshToken(refreshToken)
+////                .expiration(date.toString())
+////                .build();
+////
+////        refreshRepository.save(refresh);
+////    }
 //
-//        Refresh refresh = Refresh.builder()
-//                .userId(userId)
-//                .refreshToken(refreshToken)
-//                .expiration(date.toString())
-//                .build();
-//
-//        refreshRepository.save(refresh);
+//    private String createJwt(String category, String id, Role role, String loginType, long expiredMs) {
+//        return Jwts.builder()
+//                .claim("category", category)
+//                .claim("sub", String.valueOf(id))
+//                .claim("role", role)
+//                .claim("loginType", loginType)
+//                .issuedAt(new Date(System.currentTimeMillis()))
+//                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+//                .signWith(secretKey)
+//                .compact();
 //    }
-
-    private String createJwt(String category, String id, Role role, String loginType, long expiredMs) {
-        return Jwts.builder()
-                .claim("category", category)
-                .claim("sub", String.valueOf(id))
-                .claim("role", role)
-                .claim("loginType", loginType)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiredMs))
-                .signWith(secretKey)
-                .compact();
-    }
-}
+//}
