25e4d6c99784c4c7bf012eaba8a793577db87474
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:18:09 +0900

    refactor(marking): 코드 리팩토링

--- src/main/java/com/oceaneeda/server/domain/marking/domain/Marking.java
+++ src/main/java/com/oceaneeda/server/domain/marking/domain/Marking.java
@@ -1,10 +1,10 @@
 package com.oceaneeda.server.domain.marking.domain;
 
+import com.oceaneeda.server.domain.file.domain.File;
 import com.oceaneeda.server.domain.marking.domain.value.Category;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.oceaneeda.server.domain.user.domain.User;
+import com.oceaneeda.server.global.constants.MongoCollections;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
@@ -15,27 +15,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 
-
-
-/**
- * Marking 엔티티는 사용자 제출한 마킹 데이터를 저장합니다.
- * - userId: 사용자의 ID
- * - 지역 정보 (regionId, GeoPoint)
- * - 분류 정보 (category, trashTypes)
- * - 승인 여부 (isApproved)
- */
-@Document(collection = "markings")
+@Document(collection = MongoCollections.MARKING)
 @Getter
-@Setter
-//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Marking {
 
     @Id
     private ObjectId id;
 
-    private String userId;
+    private ObjectId userId;
 
-    private String regionId;
+    private ObjectId regionId;
 
     private String title;
 
@@ -56,11 +46,40 @@ public class Marking {
 
     private Category category;
 
+    @Builder
+    public Marking(ObjectId regionId,
+                   String title,
+                   String content,
+                   String poster,
+                   List<String> trashTypes,
+                   GeoJsonPoint location,
+                   List<File> files,
+                   Boolean isApproved,
+                   Category category) {
+        this.regionId = regionId;
+        this.title = title;
+        this.content = content;
+        this.poster = poster;
+        this.trashTypes = trashTypes;
+        this.location = location;
+        this.files = files;
+        this.isApproved = isApproved;
+        this.category = category;
+    }
 
-    @Getter
-    @Setter
-    public static class File {
-        private String name;
-        private String path;
+    public void update(Marking marking) {
+        if (marking.regionId != null) this.regionId = marking.regionId;
+        if (marking.title != null) this.title = marking.title;
+        if (marking.content != null) this.content = marking.content;
+        if (marking.poster != null) this.poster = marking.poster;
+        if (marking.trashTypes != null) this.trashTypes = marking.trashTypes;
+        if (marking.location != null) this.location = marking.location;
+        if (marking.files != null) this.files = marking.files;
+        if (marking.isApproved != null) this.isApproved = marking.isApproved;
+        if (marking.category != null) this.category = marking.category;
+    }
+
+    public void updateUser(User user) {
+        this.userId = user.getId();
     }
 }
