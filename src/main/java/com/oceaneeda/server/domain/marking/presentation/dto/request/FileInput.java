98c2e4a196878d44ab565ca4789199f2aa142d43
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:17:47 +0900

    refactor(file): 코드 리팩토링

--- src/main/java/com/oceaneeda/server/domain/marking/presentation/dto/request/FileInput.java
+++ src/main/java/com/oceaneeda/server/domain/marking/presentation/dto/request/FileInput.java
@@ -1,9 +1,17 @@
 package com.oceaneeda.server.domain.marking.presentation.dto.request;
 
-import org.springframework.web.multipart.MultipartFile;
+import com.oceaneeda.server.domain.file.domain.File;
 
 public record FileInput (
         String name,
-        MultipartFile file
+        String path,
+        int order
 ) {
+    public File toDomain() {
+        return File.builder()
+                .name(name)
+                .path(path)
+                .order(order)
+                .build();
+    }
 }
