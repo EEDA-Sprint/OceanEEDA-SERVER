67ed6bc71eb26bb2ceaa74add1d7bc719ecebe6b
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:09:53 +0900

    feat(auth): ForEntity 구현

---
+++ src/main/java/com/oceaneeda/server/domain/auth/service/annotation/ForEntity.java
@@ -0,0 +1,13 @@
+package com.oceaneeda.server.domain.auth.service.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(TYPE)
+public @interface ForEntity {
+    Class<?> value();
+}
