b51c95f5ee5c7ec925f01e4c75823fa6f0759304
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:26:12 +0900

    remove(auth): CreateMarkingInput

--- src/main/java/com/oceaneeda/server/domain/marking/presentation/dto/request/CreateMarkingInput.java
+++ src/main/java/com/oceaneeda/server/domain/marking/presentation/dto/request/CreateMarkingInput.java
@@ -1,17 +1,17 @@
-package com.oceaneeda.server.domain.marking.presentation.dto.request;
-
-import com.oceaneeda.server.domain.marking.domain.value.Category;
-
-import java.util.List;
-
-public record CreateMarkingInput(
-        String regionId,
-        String title,
-        String content,
-        String poster,
-        List<String> trashTypes,
-        GeoJsonPointInput location,
-        List<FileInput> files,
-        Boolean isApproved,
-        Category category
-) {}
+//package com.oceaneeda.server.domain.marking.presentation.dto.request;
+//
+//import com.oceaneeda.server.domain.marking.domain.value.Category;
+//
+//import java.util.List;
+//
+//public record CreateMarkingInput(
+//        String regionId,
+//        String title,
+//        String content,
+//        String poster,
+//        List<String> trashTypes,
+//        GeoJsonPointInput location,
+//        List<FileInput> files,
+//        Boolean isApproved,
+//        Category category
+//) {}
