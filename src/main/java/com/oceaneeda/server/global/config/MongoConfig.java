cf50405e5a8f7981b67f09df492910c6d0384386
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:24:17 +0900

    feat(mongo): mongo 관련 설정 구현

--- src/main/java/com/oceaneeda/server/global/config/MongoConfig.java
+++ src/main/java/com/oceaneeda/server/global/config/MongoConfig.java
@@ -4,14 +4,23 @@ import com.oceaneeda.server.global.converter.DateToLocalDateTimeKstConverter;
 import com.oceaneeda.server.global.converter.LocalDateTimeToDateKstConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Arrays;
 
 @Configuration
 @EnableMongoAuditing
+//@EnableTransactionManagement 기본적으로 활성되어 있음
 public class MongoConfig {
+
+    @Bean
+    public MongoTransactionManager transactionManager(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTransactionManager(mongoDatabaseFactory);
+    }
 
     @Bean
     public MongoCustomConversions customConversions(
---
+++ src/main/java/com/oceaneeda/server/global/constants/MongoCollections.java
@@ -0,0 +1,8 @@
+package com.oceaneeda.server.global.constants;
+
+public class MongoCollections {
+    public static final String MARKING = "markings";
+    public static final String USER = "users";
+    public static final String REGION = "regions";
+    public static final String TRASHTYPE = "trash_types";
+}
