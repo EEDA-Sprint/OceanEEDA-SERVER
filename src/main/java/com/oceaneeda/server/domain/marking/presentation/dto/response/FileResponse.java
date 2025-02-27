903759c09313f01824f2bfe1c556dde52fec41b6
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:17:52 +0900

    refactor(file): 코드 리팩토링

--- src/main/java/com/oceaneeda/server/domain/marking/presentation/dto/response/FileResponse.java
+++ src/main/java/com/oceaneeda/server/domain/marking/presentation/dto/response/FileResponse.java
@@ -1,17 +1,19 @@
 package com.oceaneeda.server.domain.marking.presentation.dto.response;
 
-import com.oceaneeda.server.domain.marking.domain.Marking;
+import com.oceaneeda.server.domain.file.domain.File;
 import lombok.Builder;
 
 @Builder
 public record FileResponse(
         String name,
-        String path
+        String path,
+        int order
 ) {
-    public static FileResponse from(Marking.File file) {
+    public static FileResponse from(File file) {
         return FileResponse.builder()
                 .name(file.getName())
                 .path(file.getPath())
+                .order(file.getOrder())
                 .build();
     }
 }
