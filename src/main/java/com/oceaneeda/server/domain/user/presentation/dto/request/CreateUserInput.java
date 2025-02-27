c5d468abd83ba71daf160ee9e80259f3b81bc483
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:17:30 +0900

    fix(user): 코드 리팩토링

--- src/main/java/com/oceaneeda/server/domain/user/presentation/dto/request/CreateUserInput.java
+++ src/main/java/com/oceaneeda/server/domain/user/presentation/dto/request/CreateUserInput.java
@@ -1,7 +1,20 @@
 package com.oceaneeda.server.domain.user.presentation.dto.request;
 
+import com.oceaneeda.server.domain.user.domain.User;
+import com.oceaneeda.server.domain.user.domain.value.Role;
+import com.oceaneeda.server.domain.user.domain.value.Type;
+
 public record CreateUserInput(
         String username,
         String email,
         String password
-) {}
+) {
+    public User toEntity() {
+        return User.builder()
+                .username(username)
+                .email(email)
+                .socialLoginType(Type.NONE)
+                .role(Role.USER)
+                .build();
+    }
+}
