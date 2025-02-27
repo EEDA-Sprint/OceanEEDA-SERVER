2ece887b50de19d685fbcc7a8efb4b0c3a056007
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:21:03 +0900

    feat(auth): 코드 리팩토링

--- src/main/java/com/oceaneeda/server/domain/auth/presentation/dto/response/TokenResponse.java
+++ src/main/java/com/oceaneeda/server/domain/auth/presentation/dto/response/TokenResponse.java
@@ -1,7 +1,15 @@
 package com.oceaneeda.server.domain.auth.presentation.dto.response;
 
+import com.oceaneeda.server.domain.auth.domain.Token;
+
 public record TokenResponse(
         String accessToken,
         String refreshToken
 ) {
+    public static TokenResponse from(Token token) {
+        return new TokenResponse(
+                token.accessToken(),
+                token.refreshToken()
+        );
+    }
 }
