bdd2397f4696424b81a76247bafe23612c92377e
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:15:02 +0900

    fix(aop): aop 로직 수정

--- src/main/java/com/oceaneeda/server/domain/auth/aop/AuthAspect.java
+++ src/main/java/com/oceaneeda/server/domain/auth/aop/AuthAspect.java
@@ -2,18 +2,21 @@ package com.oceaneeda.server.domain.auth.aop;
 
 import com.oceaneeda.server.domain.auth.annotation.AdminOnly;
 import com.oceaneeda.server.domain.auth.annotation.Authenticated;
-import com.oceaneeda.server.domain.auth.annotation.CheckOwnership;
-import com.oceaneeda.server.domain.auth.repository.AuthRepository;
-import com.oceaneeda.server.domain.marking.domain.repository.MarkingRepository;
+import com.oceaneeda.server.domain.auth.annotation.OwnerOnly;
+import com.oceaneeda.server.domain.auth.annotation.PublicOrOwner;
+import com.oceaneeda.server.domain.auth.exception.UserNotAdminException;
+import com.oceaneeda.server.domain.auth.registry.AccessCheckerRegistry;
+import com.oceaneeda.server.domain.auth.registry.OwnerCheckerRegistry;
+import com.oceaneeda.server.domain.auth.service.core.AccessChecker;
+import com.oceaneeda.server.domain.auth.service.core.OwnerChecker;
+import com.oceaneeda.server.domain.auth.service.implementation.AuthReader;
 import com.oceaneeda.server.domain.user.domain.User;
 import com.oceaneeda.server.domain.user.domain.value.Role;
-import com.oceaneeda.server.global.exception.AccessDeniedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -22,36 +25,42 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AuthAspect {
 
-    private final AuthRepository authRepository;
-    private final MarkingRepository markingRepository;
+    private final AuthReader authReader;
+    private final OwnerCheckerRegistry ownerCheckerRegistry;
+    private final AccessCheckerRegistry accessCheckerRegistry;
 
     @Before("@annotation(authenticated)")
     public void checkAuthenticated(Authenticated authenticated) {
-        authRepository.getCurrentUser();
+        authReader.getCurrentUser();
     }
 
-    @Before("@annotation(checkOwnership)")
-    public void checkOwnership(JoinPoint joinPoint, CheckOwnership checkOwnership) {
-        User currentUser = authRepository.getCurrentUser();
-        ObjectId markingId = (ObjectId) joinPoint.getArgs()[0];
-        log.warn("체크 오너쉽 : "+markingId);
-        log.warn("현재 유저 : "+currentUser.getId());
+    @Before("@annotation(ownerOnly)")
+    public void checkOwner(JoinPoint joinPoint, OwnerOnly ownerOnly) {
+        User currentUser = authReader.getCurrentUser();
+        Object resourceId = joinPoint.getArgs()[0];
 
-        if (currentUser.getRole() != Role.ROLE_ADMIN && !isOwner(markingId, currentUser.getId().toHexString())) {
-            throw new AccessDeniedException("권한이 없습니다.");
-        }
+        OwnerChecker<Object> ownerChecker = ownerCheckerRegistry.getChecker(ownerOnly.resourceType());
+
+        ownerChecker.checkOwner(resourceId, currentUser);
     }
+
+    @Before("@annotation(publicOrOwner)")
+    public void checkPublicOrOwner(JoinPoint joinPoint, PublicOrOwner publicOrOwner) {
+        User currentUser = authReader.getCurrentUser();
+        Object resourceId = joinPoint.getArgs()[0];
+
+        AccessChecker<Object> accessChecker = accessCheckerRegistry.getChecker(publicOrOwner.resourceType());
+
+        accessChecker.checkAccess(resourceId, currentUser);
+    }
+
 
     @Before("@annotation(adminOnly)")
     public void checkAdmin(AdminOnly adminOnly) {
-        User currentUser = authRepository.getCurrentUser();
+        User currentUser = authReader.getCurrentUser();
 
-        if (currentUser.getRole() != Role.ROLE_ADMIN) {
-            throw new AccessDeniedException("관리자 권한이 필요합니다.");
+        if (currentUser.getRole() != Role.ADMIN) {
+            throw new UserNotAdminException();
         }
-    }
-
-    private boolean isOwner(ObjectId markingId, String userId) {
-        return markingRepository.existsByIdAndUserId(markingId, userId);
     }
 }
