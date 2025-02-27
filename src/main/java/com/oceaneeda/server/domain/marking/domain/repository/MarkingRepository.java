eca73c82e5f74596a6b1757f848860090d363d08
Yunchan-Oh <oyunchan9@gmail.com>
2025-02-27 10:18:22 +0900

    refactor(marking): 코드 리팩토링

--- src/main/java/com/oceaneeda/server/domain/marking/domain/repository/MarkingRepository.java
+++ src/main/java/com/oceaneeda/server/domain/marking/domain/repository/MarkingRepository.java
@@ -1,6 +1,7 @@
 package com.oceaneeda.server.domain.marking.domain.repository;
 
 import com.oceaneeda.server.domain.marking.domain.Marking;
+import com.oceaneeda.server.domain.marking.exception.MarkingNotFoundException;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -9,9 +10,14 @@ import java.util.List;
 
 @Repository
 public interface MarkingRepository extends MongoRepository<Marking, ObjectId> {
-    boolean existsByIdAndUserId(ObjectId id, String userId);
+    boolean existsByIdAndUserId(ObjectId id, ObjectId userId);
 
     List<Marking> findByIsApprovedTrue();
 
-    List<Marking> findByIsApprovedTrueOrUserId(String userId);
+    List<Marking> findByUserId(ObjectId userId);
+
+    default Marking getById(ObjectId markingId) {
+        return findById(markingId)
+                .orElseThrow(() -> new MarkingNotFoundException(markingId));
+    }
 }
