3ee4ffb84d498546715a885c166cc4f22371f881
Yunchan-Oh <oyunchan9@gmail.com>
2025-03-06 09:27:38 +0900

    refactor(auth): 커스텀 체커 추가

--- src/main/java/com/oceaneeda/server/domain/auth/annotation/OwnerOnly.java
+++ src/main/java/com/oceaneeda/server/domain/auth/annotation/OwnerOnly.java
@@ -5,6 +5,7 @@ import java.lang.annotation.*;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@CustomChecker
 public @interface OwnerOnly {
     Class<?> resourceType();
 }
--- src/main/java/com/oceaneeda/server/domain/auth/annotation/PublicOrOwner.java
+++ src/main/java/com/oceaneeda/server/domain/auth/annotation/PublicOrOwner.java
@@ -5,6 +5,7 @@ import java.lang.annotation.*;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@CustomChecker
 public @interface PublicOrOwner {
     Class<?> resourceType();
 }
