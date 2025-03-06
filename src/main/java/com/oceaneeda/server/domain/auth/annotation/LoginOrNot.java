3f3874d5f12043cec3fbbe2fe44197802a5f1472
Yunchan-Oh <oyunchan9@gmail.com>
2025-03-06 09:29:59 +0900

    feat(auth): LoginOrNot 어노테이션 구현

---
+++ src/main/java/com/oceaneeda/server/domain/auth/annotation/LoginOrNot.java
@@ -0,0 +1,10 @@
+package com.oceaneeda.server.domain.auth.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@CustomChecker
+public @interface LoginOrNot {
+}
