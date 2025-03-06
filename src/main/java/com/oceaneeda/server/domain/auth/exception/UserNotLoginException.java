f7ee3b12112f811d3593126811246d767ae16b46
Yunchan-Oh <oyunchan9@gmail.com>
2025-03-06 09:24:15 +0900

    fix(auth): 예외 메세지 수정

--- src/main/java/com/oceaneeda/server/domain/auth/exception/UserNotLoginException.java
+++ src/main/java/com/oceaneeda/server/domain/auth/exception/UserNotLoginException.java
@@ -5,6 +5,6 @@ import org.springframework.http.HttpStatus;
 
 public class UserNotLoginException extends OceaneedaException {
     public UserNotLoginException() {
-        super(HttpStatus.UNAUTHORIZED, "AUTH-401-5", "유저가 어드민이 아닙니다.");
+        super(HttpStatus.UNAUTHORIZED, "AUTH-401-5", "유저가 로그인하지 않았습니다.");
     }
 }
