8d4c9a01b85568bba476e9e1241f7c2f1a060a89
Yunchan-Oh <oyunchan9@gmail.com>
2025-03-10 08:26:28 +0900

    feat(auth): 필드 제한 체커 구현

---
+++ src/main/java/com/oceaneeda/server/domain/auth/annotation/FieldPermission.java
@@ -0,0 +1,12 @@
+package com.oceaneeda.server.domain.auth.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@CustomChecker
+public @interface FieldPermission {
+    Class<?> inputType();
+    String fieldName();
+}
