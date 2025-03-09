3911f3adfc0076c57514a26d604b49d61a00f5e3
Yunchan-Oh <oyunchan9@gmail.com>
2025-03-10 08:25:20 +0900

    feat(auth): 필드 제한 체커 구현

--- src/main/java/com/oceaneeda/server/domain/auth/aop/AuthAspect.java
+++ src/main/java/com/oceaneeda/server/domain/auth/aop/AuthAspect.java
@@ -1,15 +1,15 @@
 package com.oceaneeda.server.domain.auth.aop;
 
-import com.oceaneeda.server.domain.auth.annotation.AdminOnly;
-import com.oceaneeda.server.domain.auth.annotation.Authenticated;
-import com.oceaneeda.server.domain.auth.annotation.OwnerOnly;
-import com.oceaneeda.server.domain.auth.annotation.PublicOrOwner;
+import com.oceaneeda.server.domain.auth.annotation.*;
 import com.oceaneeda.server.domain.auth.exception.UserNotAdminException;
 import com.oceaneeda.server.domain.auth.registry.AccessCheckerRegistry;
+import com.oceaneeda.server.domain.auth.registry.FieldCheckerRegistry;
 import com.oceaneeda.server.domain.auth.registry.OwnerCheckerRegistry;
 import com.oceaneeda.server.domain.auth.service.core.AccessChecker;
+import com.oceaneeda.server.domain.auth.service.core.FieldChecker;
 import com.oceaneeda.server.domain.auth.service.core.OwnerChecker;
 import com.oceaneeda.server.domain.auth.service.implementation.AuthReader;
+import com.oceaneeda.server.domain.auth.util.JoinPointArgumentExtractor;
 import com.oceaneeda.server.domain.user.domain.User;
 import com.oceaneeda.server.domain.user.domain.value.Role;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +28,7 @@ public class AuthAspect {
     private final AuthReader authReader;
     private final OwnerCheckerRegistry ownerCheckerRegistry;
     private final AccessCheckerRegistry accessCheckerRegistry;
+    private final FieldCheckerRegistry fieldCheckerRegistry;
 
     @Before("@annotation(authenticated)")
     public void checkAuthenticated(Authenticated authenticated) {
@@ -37,7 +38,7 @@ public class AuthAspect {
     @Before("@annotation(ownerOnly)")
     public void checkOwner(JoinPoint joinPoint, OwnerOnly ownerOnly) {
         User currentUser = authReader.getCurrentUser();
-        Object resourceId = joinPoint.getArgs()[0];
+        Object resourceId = JoinPointArgumentExtractor.findArgumentByObjectId(joinPoint);
 
         OwnerChecker<Object> ownerChecker = ownerCheckerRegistry.getChecker(ownerOnly.resourceType());
 
@@ -47,11 +48,21 @@ public class AuthAspect {
     @Before("@annotation(publicOrOwner)")
     public void checkPublicOrOwner(JoinPoint joinPoint, PublicOrOwner publicOrOwner) {
         User currentUser = authReader.getCurrentUser();
-        Object resourceId = joinPoint.getArgs()[0];
+        Object resourceId = JoinPointArgumentExtractor.findArgumentByObjectId(joinPoint);
 
         AccessChecker<Object> accessChecker = accessCheckerRegistry.getChecker(publicOrOwner.resourceType());
 
         accessChecker.checkAccess(resourceId, currentUser);
+    }
+
+    @Before("@annotation(fieldPermission)")
+    public void checkFieldPermission(JoinPoint joinPoint, FieldPermission fieldPermission) {
+        User currentUser = authReader.getCurrentUser();
+        Object inputObject = JoinPointArgumentExtractor.findArgumentByType(joinPoint, fieldPermission.inputType());
+
+        FieldChecker<Object> checker = fieldCheckerRegistry.getChecker(fieldPermission.inputType(), fieldPermission.fieldName());
+
+        checker.checkField(inputObject, currentUser);
     }
 
 
