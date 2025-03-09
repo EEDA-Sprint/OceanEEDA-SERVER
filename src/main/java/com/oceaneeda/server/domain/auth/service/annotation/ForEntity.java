2e1ef5ee9cdd598a2be3a6c49936aa41cdf57df3
Yunchan-Oh <oyunchan9@gmail.com>
2025-03-10 08:43:14 +0900

    refactor(auth): @Documented 어노테이션 추가

--- src/main/java/com/oceaneeda/server/domain/auth/service/annotation/ForEntity.java
+++ src/main/java/com/oceaneeda/server/domain/auth/service/annotation/ForEntity.java
@@ -1,5 +1,6 @@
 package com.oceaneeda.server.domain.auth.service.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -8,6 +9,7 @@ import static java.lang.annotation.ElementType.TYPE;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(TYPE)
+@Documented
 public @interface ForEntity {
     Class<?> value();
 }
