190e3d686db0961bec7390c9ed6c89de625e9b1c
Yunchan-Oh <oyunchan9@gmail.com>
2025-03-10 08:39:31 +0900

    fix(user): 이메일 중복 시 예외 처리 추가

--- src/main/java/com/oceaneeda/server/domain/user/service/CommandUserService.java
+++ src/main/java/com/oceaneeda/server/domain/user/service/CommandUserService.java
@@ -4,6 +4,7 @@ import com.oceaneeda.server.domain.user.domain.User;
 import com.oceaneeda.server.domain.user.service.implementation.UserCreator;
 import com.oceaneeda.server.domain.user.service.implementation.UserDeleter;
 import com.oceaneeda.server.domain.user.service.implementation.UserUpdater;
+import com.oceaneeda.server.domain.user.service.implementation.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,13 +17,16 @@ public class CommandUserService {
     private final UserCreator userCreator;
     private final UserUpdater userUpdater;
     private final UserDeleter userDeleter;
+    private final UserValidator userValidator;
 
     public User createUser(User user, String password) {
+        userValidator.shouldNotExistEmail(user);
         userUpdater.updatePassword(user, password);
         return userCreator.create(user);
     }
 
     public User updateUser(User updatableUser, User user) {
+        userValidator.shouldNotExistEmail(user);
         return userUpdater.update(updatableUser, user);
     }
 
