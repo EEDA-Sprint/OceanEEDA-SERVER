42cd991b688ae2451d6b55042262e561cb3e1746
Yunchan-Oh <oyunchan9@gmail.com>
2025-03-10 15:10:13 +0900

    fix(cors): cors 설정 추가

--- src/main/java/com/oceaneeda/server/global/config/InterceptorConfig.java
+++ src/main/java/com/oceaneeda/server/global/config/InterceptorConfig.java
@@ -1,45 +1,31 @@
-//package com.oceaneeda.server.global.config;
-//
-//import com.oceaneeda.server.domain.auth.interceptor.AuthInterceptor;
-//import com.oceaneeda.server.domain.auth.service.implementation.AuthUpdater;
-//import com.oceaneeda.server.domain.auth.util.JwtParser;
-//import com.oceaneeda.server.domain.user.service.implementation.UserReader;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class InterceptorConfig implements WebMvcConfigurer {
-//
-//    private static final String ALLOW_ALL_PATH = "/**";
-//    private static final String ALLOWED_METHODS = "*";
-//    private static final String MAIN_SERVER_DOMAIN = "https://oceaneeda.com";
-//    private static final String FRONTEND_LOCALHOST = "http://localhost:3000";
-//    private static final String HTTPS_FRONTEND_LOCALHOST = "https://localhost:3000";
-//
-//    private final UserReader userReader;
-//    private final AuthUpdater authUpdater;
-//    private final JwtParser jwtParser;
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping(ALLOW_ALL_PATH)
-//                .allowedMethods(ALLOWED_METHODS)
-//                .allowedOrigins(
-//                        MAIN_SERVER_DOMAIN,
-//                        FRONTEND_LOCALHOST,
-//                        HTTPS_FRONTEND_LOCALHOST
-//                )
-//                .exposedHeaders(AUTHORIZATION);
-//    }
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new AuthInterceptor(userReader, authUpdater, jwtParser));
-//    }
-//}
+package com.oceaneeda.server.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private static final String ALLOW_ALL_PATH = "/**";
+    private static final String ALLOWED_METHODS = "*";
+    private static final String MAIN_SERVER_DOMAIN = "https://oceaneeda.com";
+    private static final String FRONTEND_LOCALHOST = "http://localhost:3000";
+    private static final String HTTPS_FRONTEND_LOCALHOST = "https://localhost:3000";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(ALLOW_ALL_PATH)
+                .allowedMethods(ALLOWED_METHODS)
+                .allowedOrigins(
+                        MAIN_SERVER_DOMAIN,
+                        FRONTEND_LOCALHOST,
+                        HTTPS_FRONTEND_LOCALHOST
+                )
+                .exposedHeaders(AUTHORIZATION);
+    }
+}
