a481d9e95b2418758d7c60b27bfe5dc7b7228aa2
Yunchan-Oh <oyunchan9@gmail.com>
2025-03-13 15:52:45 +0900

    fix(marking): isApproved 필드 검사 수정

--- src/main/java/com/oceaneeda/server/domain/marking/service/implementation/MarkingIsApprovedFieldChecker.java
+++ src/main/java/com/oceaneeda/server/domain/marking/service/implementation/MarkingIsApprovedFieldChecker.java
@@ -16,7 +16,7 @@ import static com.oceaneeda.server.domain.user.domain.value.Role.ADMIN;
 public class MarkingIsApprovedFieldChecker implements FieldChecker<MarkingInput> {
     @Override
     public void checkField(MarkingInput input, User currentUser) {
-        if (input.isApproved() != null && currentUser.getRole() != ADMIN) {
+        if (input.isApproved() && currentUser.getRole() != ADMIN) {
             throw new UserNotAdminException();
         }
     }
